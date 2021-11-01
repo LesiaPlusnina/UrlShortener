1005c62dd5b6ee74e7aa0dd8763b6a7cc25358c1
LesiaPlusnina <alis.pl.94@gmail.com>
2021-11-01 14:35:41 +0300

    newviews.py

--- project_1/shortenersite/views.py
+++ project_1/shortenersite/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render, redirect
 import uuid
 from .models import Url
 from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        long_url = request.POST['long_url']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(long_url=long_url, token=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    data = Url.objects.get(token=pk)
+    return redirect(data.long_url)
